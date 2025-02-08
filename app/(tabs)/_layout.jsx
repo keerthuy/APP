e5135ce6c65f027e5ebc2f325aed8be1636b614c
keerthuy <keerththanan109@gmail.com>
2025-02-08 16:48:13 +0530

    add navigation

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,14 +1,34 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
-
+import Ionicons from '@expo/vector-icons/Ionicons';
 export default function TabLayout() {
   return (
   <Tabs>
-    <Tabs.Screen name="home"/>
-    <Tabs.Screen name="explore"/>
-    <Tabs.Screen name="progress"/>
-    <Tabs.Screen name="profile"/>
+    <Tabs.Screen name="home"
+    options={{
+      tabBarIcon:({color,size}) => <Ionicons name="home-outline" size={size} color={color} />,
+      tabBarLabel : 'Home'
+    }}
+    />
+    <Tabs.Screen name="explore"
+    options={{
+      tabBarIcon:({color,size}) => <Ionicons name="search-outline" size={size} color={color} />,
+      tabBarLabel : 'Search'
+    }}
+    />
+    <Tabs.Screen name="progress"
+    options={{
+      tabBarIcon:({color,size}) => <Ionicons name="analytics-outline" size={size} color={color} />,
+      tabBarLabel : 'Progress'
+    }}
+    />
+    <Tabs.Screen name="profile"
+    options={{
+      tabBarIcon:({color,size}) => <Ionicons name="person-circle-outline" size={size} color={color} />,
+      tabBarLabel : 'Profile'
+    }}
+    />
   </Tabs>
   )
 }
